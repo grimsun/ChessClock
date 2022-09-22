af8053663e54cf6b2d11af8d82dde0b1ca3fbf91
grimsun <grimsunnn@gmail.com>
2022-09-21 22:44:54 -0700

    improve switches between activities

--- app/src/main/java/com/lab/chessclock/SettingsActivity.kt
+++ app/src/main/java/com/lab/chessclock/SettingsActivity.kt
@@ -3,7 +3,6 @@ package com.lab.chessclock
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NavUtils
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +19,8 @@ class SettingsActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
-                NavUtils.navigateUpFromSameTask(this)
+                finish()
+                //NavUtils.navigateUpFromSameTask(this)
                 true
             }
             else -> super.onOptionsItemSelected(item)
