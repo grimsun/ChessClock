6b78d4f9a49e251e2279fe41b44a5be2d2b9cbe6
grimsun <grimsunnn@gmail.com>
2022-09-24 23:07:42 -0700

    improve settings

--- app/src/main/java/com/lab/chessclock/SettingsFragment.kt
+++ app/src/main/java/com/lab/chessclock/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.lab.chessclock
 
 import android.os.Bundle
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+        val listPreference: ListPreference? = findPreference("orientation")
+        listPreference?.summaryProvider = ListPreference.SimpleSummaryProvider.getInstance()
     }
 }
